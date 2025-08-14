dd7942530c610e8e5c09e3c2cff9905fb6b9b386
SantinoGiordano <santinogiordano13@gmail.com>
2025-08-14 13:50:44 -0400

    simple set up

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     hello world
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20">
+     Guess Here
+     <input type="text" className="border border-gray-300 rounded-md p-2 w-full max-w-md" placeholder="Type your guess..." />
     </div>
   );
 }
