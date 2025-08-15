ab6ae5b3fbdd324833cf726c2163199bfc28142a
SantinoGiordano <santinogiordano13@gmail.com>
2025-08-15 09:11:24 -0400

    added useStates of guess and words also use effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [word, setWord] = useState("");
+  const [guess, setGuess] = useState("");
+
+  useEffect(() => {
+
+  }, []);
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20">
      Guess Here
