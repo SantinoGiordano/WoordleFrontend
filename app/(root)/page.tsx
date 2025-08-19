8b03bbe231d62416b9fd11c0e0a2bccce1de9370
SantinoGiordano <santinogiordano13@gmail.com>
2025-08-19 12:58:07 -0400

    main page for game

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <div>
+        <button>Play Game</button>
+        <button>Speed Round</button>
+        <button></button>
+      </div>
+    </>
+  );
+}
