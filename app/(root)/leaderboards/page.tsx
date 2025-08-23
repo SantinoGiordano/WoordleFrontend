284af0119e387b52a42e8995b8b18fdb07c84767
SantinoGiordano <santinogiordano13@gmail.com>
2025-08-23 09:10:44 -0400

    scorboard data start

--- app/(root)/leaderboards/page.tsx
+++ app/(root)/leaderboards/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+'use client'
 
-const page = () => {
+import { User } from '@/types/types';
+import React, { useState, useEffect } from 'react';
+
+const Page = () => {
+  const [items, setItems] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/users")
+      .then((res) => res.json())
+      .then((userInfo: User[]) => {
+        setItems(userInfo);
+      });
+  }, []);
+
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <h1>Leaderboards Page</h1>
+      {items.map((item) => (
+        <div key={item._id}>{item.username}</div>
+      ))}
+    </div>
+  );
+};
 
-export default page
+export default Page;
