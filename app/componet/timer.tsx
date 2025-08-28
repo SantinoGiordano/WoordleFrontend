d46345abeff97ecc0c2c74a374ac8f4431eed0dc
SantinoGiordano <santinogiordano13@gmail.com>
2025-08-28 12:49:23 -0400

    eslint rule changed typing

--- app/componet/timer.tsx
+++ app/componet/timer.tsx
@@ -5,6 +5,7 @@ export default function TimerProgress({ progress }: { progress: number }) {
     <div className="flex flex-col items-center gap-2 w-full max-w-xs">
       <div
         className="radial-progress"
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         style={{ ["--value" as any]: progress } as React.CSSProperties}
         aria-valuenow={progress}
         role="progressbar"
