06a2e3bfb7fe27901e0f5923e0dc735eb2802a92
SantinoGiordano <santinogiordano13@gmail.com>
2025-09-10 18:38:09 -0400

    added eslint rule for any

--- app/(root)/speedRun/page.tsx
+++ app/(root)/speedRun/page.tsx
@@ -79,6 +79,7 @@ export default function Home() {
       <div className="flex flex-col items-center gap-2 w-full max-w-xs">
         <div
           className="radial-progress"
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
           style={{ ["--value" as any]: progress } as React.CSSProperties}
           aria-valuenow={progress}
           role="progressbar"
