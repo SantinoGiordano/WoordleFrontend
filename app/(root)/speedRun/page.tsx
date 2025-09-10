4549008f438178abed38fb5efab30244152c1cc5
SantinoGiordano <santinogiordano13@gmail.com>
2025-09-10 18:57:58 -0400

    removed the use of any

--- app/(root)/speedRun/page.tsx
+++ app/(root)/speedRun/page.tsx
@@ -79,8 +79,8 @@ export default function Home() {
       <div className="flex flex-col items-center gap-2 w-full max-w-xs">
         <div
           className="radial-progress"
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          style={{ ["--value" as any]: progress } as React.CSSProperties}
+
+          style={{ "--value": progress } as React.CSSProperties & { "--value": number }}
           aria-valuenow={progress}
           role="progressbar"
         >
